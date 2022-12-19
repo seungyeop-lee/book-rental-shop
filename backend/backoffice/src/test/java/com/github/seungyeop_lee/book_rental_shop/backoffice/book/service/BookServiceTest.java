3f1abe126f4eba255a0e2f4adfa959d2f419e94b
seungyeop-lee <xbeast7@gmail.com>
2022-12-19 23:32:35 +0900

    BookServiceTest를 단위 테스트로 변경

--- backend/backoffice/src/test/java/com/github/seungyeop_lee/book_rental_shop/backoffice/book/service/BookServiceTest.java
+++ backend/backoffice/src/test/java/com/github/seungyeop_lee/book_rental_shop/backoffice/book/service/BookServiceTest.java
@@ -1,96 +1,101 @@
 package com.github.seungyeop_lee.book_rental_shop.backoffice.book.service;
 
+import com.github.seungyeop_lee.book_rental_shop.backoffice.book.domain.Book;
+import com.github.seungyeop_lee.book_rental_shop.backoffice.book.domain.BookRepository;
 import com.github.seungyeop_lee.book_rental_shop.backoffice.book.service.param.BookCreateParameter;
 import com.github.seungyeop_lee.book_rental_shop.backoffice.book.service.param.BookUpdateParameter;
 import com.github.seungyeop_lee.book_rental_shop.backoffice.book.service.result.BookReadResult;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.orm.jpa.JpaObjectRetrievalFailureException;
-import org.springframework.transaction.annotation.Transactional;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class BookServiceTest {
 
-    @Autowired
+    @InjectMocks
     private BookService bookService;
 
-    // fixture
-    private BookCreateParameter createParam;
-    private BookUpdateParameter updateParam;
+    @Mock
+    private BookRepository bookRepository;
 
-    @BeforeEach
-    void setUp() {
-        createParam = new BookCreateParameter() {{
-            setTitle("a");
-            setIsbn("abcd1234");
-        }};
-        updateParam = new BookUpdateParameter() {{
-            setTitle("b");
-            setIsbn("bcde2345");
-        }};
-    }
+    @Mock
+    private Book book;
 
     @DisplayName("책 등록")
     @Test
-    @Transactional
     void registerBook() {
+        //given
+        Long expectBookId = 100L;
+        when(book.getId()).thenReturn(expectBookId);
+        when(bookRepository.save(any())).thenReturn(book);
+
         //when
-        Long bookId = bookService.registerBook(createParam);
+        Long bookId = bookService.registerBook(new BookCreateParameter() {{
+            setTitle("a");
+            setIsbn("abcd1234");
+        }});
 
         //then
-        assertThat(bookId).isNotNull();
+        assertThat(bookId).isEqualTo(expectBookId);
     }
 
     @DisplayName("책 조회")
     @Test
-    @Transactional
     void readBook() {
         //given
-        Long bookId = bookService.registerBook(createParam);
+        Long bookId = 100L;
+        String title = "a";
+        String isbn = "b";
+
+        when(book.getId()).thenReturn(bookId);
+        when(book.getTitle()).thenReturn(title);
+        when(book.getIsbn()).thenReturn(isbn);
+        when(bookRepository.getReferenceById(bookId)).thenReturn(book);
 
         //when
         BookReadResult result = bookService.readBook(bookId);
 
         //then
-        assertThat(result.getId()).isNotNull();
-        assertThat(result.getTitle()).isEqualTo(createParam.getTitle());
-        assertThat(result.getIsbn()).isEqualTo(createParam.getIsbn());
+        assertThat(result.getId()).isEqualTo(bookId);
+        assertThat(result.getTitle()).isEqualTo(title);
+        assertThat(result.getIsbn()).isEqualTo(isbn);
     }
 
     @DisplayName("책 정보 수정")
     @Test
-    @Transactional
     void updateBook() {
         //given
-        Long bookId = bookService.registerBook(createParam);
+        Long bookId = 100L;
+        when(bookRepository.getReferenceById(bookId)).thenReturn(book);
 
         //when
-        bookService.updateBook(bookId, updateParam);
+        bookService.updateBook(bookId, new BookUpdateParameter() {{
+            setTitle("b");
+            setIsbn("bcde2345");
+        }});
 
         //then
-        BookReadResult result = bookService.readBook(bookId);
-        assertThat(result.getId()).isNotNull();
-        assertThat(result.getTitle()).isEqualTo(updateParam.getTitle());
-        assertThat(result.getIsbn()).isEqualTo(updateParam.getIsbn());
+        verify(book).update(any());
     }
 
     @DisplayName("책 삭제")
     @Test
-    @Transactional
     void deleteBook() {
         //given
-        Long bookId = bookService.registerBook(createParam);
+        Long bookId = 100L;
 
         //when
         bookService.deleteBook(bookId);
-        
+
         //then
-        assertThrows(JpaObjectRetrievalFailureException.class, () -> bookService.readBook(bookId));
+        verify(bookRepository).deleteById(bookId);
     }
 }
