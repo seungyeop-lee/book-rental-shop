5c5213f7e6315e7f571445080a2107eccd74c1bf
seungyeop-lee <xbeast7@gmail.com>
2022-12-25 16:57:39 +0900

    health check 추가

---
+++ backend/backoffice/runner/rental-manager/src/main/java/com/github/seungyeop_lee/book_rental_shop/backoffice/rental/healthcheck/HealthCheckController.java
@@ -0,0 +1,12 @@
+package com.github.seungyeop_lee.book_rental_shop.backoffice.rental.healthcheck;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
